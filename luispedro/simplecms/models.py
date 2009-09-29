6b427d3225db2bfbc5f090bbf0ba3d79a2f0f899
Luís Pedro Coelho <lpc@cmu.edu>
2009-09-29 19:23:27 -0400

    slugs they are called

--- luispedro/simplecms/models.py
+++ luispedro/simplecms/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(u'category', max_length=255)
+    slug = models.SlugField(u'slug')
 
 class Article(models.Model):
     title = models.CharField(u'title', max_length=255)
+    slug = models.SlugField(u'slug', max_length=255)
     categories = models.ManyToManyField(Category)
-    urls = models.CharField(u'url', max_length=255)
     content = models.TextField(u'content')
 
--- luispedro/simplecms/views.py
+++ luispedro/simplecms/views.py
@@ -1,10 +1,10 @@
 from .models import Article, Category
 from django.shortcuts import get_object_or_404, render_to_response
 
-def article(request, url):
-    art = get_object_or_404(Article, url=url)
+def article(request, slug):
+    article = get_object_or_404(Article, slug=slug)
     return render_to_response(
-                'article/article.html',
+                'simplecms/article.html',
                 {
                     'article' : article,
                 })
