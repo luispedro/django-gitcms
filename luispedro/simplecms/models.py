e18205ed1f4718e508cac65b0a57e9e1a25e953a
Luís Pedro Coelho <lpc@cmu.edu>
2009-09-29 17:58:34 -0400

    "compile"

--- luispedro/simplecms/models.py
+++ luispedro/simplecms/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
 class Article(models.Model):
     title = models.CharField(u'title', max_length=255)
-    categories = models.ManyToMany(Category, u'categories')
+    categories = models.ManyToManyField(Category)
     urls = models.CharField(u'url', max_length=255)
     content = models.TextField(u'content')
 
