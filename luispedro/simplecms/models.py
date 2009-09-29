56ccfc70900426905ae25dbab62c83e6211df0d2
Luís Pedro Coelho <lpc@cmu.edu>
2009-09-29 19:50:31 -0400

    Nice cosmetic touch

--- luispedro/simplecms/models.py
+++ luispedro/simplecms/models.py
@@ -3,10 +3,13 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(u'category', max_length=255)
     slug = models.SlugField(u'slug')
+    def __unicode__(self):
+        return self.slug
 
 class Article(models.Model):
     title = models.CharField(u'title', max_length=255)
     slug = models.SlugField(u'slug', max_length=255)
     categories = models.ManyToManyField(Category)
     content = models.TextField(u'content')
-
+    def __unicode__(self):
+        return self.title
