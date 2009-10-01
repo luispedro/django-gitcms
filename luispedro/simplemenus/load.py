0820816579c391145eec256417d823a168913058
Luís Pedro Coelho <lpc@cmu.edu>
2009-09-30 23:17:41 -0400

    loadfile() cannot clear.
    Otherwise, how would we load a whole directory?

--- luispedro/simplemenus/load.py
+++ luispedro/simplemenus/load.py
@@ -2,7 +2,11 @@ from models import Menu, MenuItem
 import os
 import yaml
 
-def loadfile(filename, clear=False):
+def clear():
+    MenuItem.objects.all().delete()
+    Menu.objects.all().delete()
+
+def loadfile(filename):
     def _parse_children(parent, parent_obj):
         if 'children' not in parent:
             return
@@ -10,9 +14,6 @@ def loadfile(filename, clear=False):
             child = MenuItem(name=ch['name'], url=ch['url'], parent=parent_obj)
             child.save()
             _parse_children(ch, child)
-    if clear:
-        MenuItem.objects.all().delete()
-        Menu.objects.all().delete()
     for menu in yaml.load_all(file(filename)):
         fake_root = MenuItem(name='<fake-root>', url='fake-root-you-shouldnt-be-seeing-this-bro')
         fake_root.save()
