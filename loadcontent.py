fa7f4f087926d90cf45eda164649a1e046a9fe2d
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-05 02:04:35 -0400

    ignore git directory

--- loadcontent.py
+++ loadcontent.py
@@ -1,6 +1,8 @@
 from os import listdir, path
 basedir = '../../website.content/'
 for app in listdir(basedir):
+    if app == '.git':
+        continue
     module = __import__(app,{},{},fromlist=['load'])
     if 'loaddir' in dir(module.load):
         module.load.loaddir(path.join(basedir, app), clear=True)
