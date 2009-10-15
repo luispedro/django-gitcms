afcff49cfac8f50e35b939add58661bc615c56cb
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-15 03:40:21 -0400

    There is only loaddir now

--- loadcontent.py
+++ loadcontent.py
@@ -6,8 +6,6 @@ for app in listdir(basedir):
     module = __import__(app,{},{},fromlist=['load'])
     if 'loaddir' in dir(module.load):
         module.load.loaddir(path.join(basedir, app), clear=True)
-    elif 'loadfile' in dir(module.load):
-        module.load.clear()
-        for item in listdir(path.join(basedir,app)):
-            module.load.loadfile(path.join(basedir,app,item))
-
+    else:
+        import warnings
+        warnings.warn("Don't know how to import '%s'" % app)
