8cf8265cc0f1832f8bd234816da7351f2b1b695c
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-15 01:03:28 -0400

    Fix ramblingsoul3 adaptation.

--- gitcms/simplemenus/templatetags/simplemenu_tags.py
+++ gitcms/simplemenus/templatetags/simplemenu_tags.py
@@ -8,3 +8,10 @@ def show_menu(menu_name):
     menu = Menu.objects.get(name=menu_name)
     return { 'items' : menu.children }
 
+@register.inclusion_tag('simplemenus/tabs.html')
+def show_tabs(menu_name):
+    menu = Menu.objects.get(name=menu_name)
+    items = menu.children
+    for it in items:
+        it.active = False
+    return { 'items' : items }
