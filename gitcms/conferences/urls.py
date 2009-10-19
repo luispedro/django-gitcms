fdfa53a1297c9ae8e7846b86582b51cdb05b0075
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-19 00:19:01 -0400

    Be more forgiving of slashes at the end

--- gitcms/conferences/urls.py
+++ gitcms/conferences/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
-    (r'^conferences/upcoming$', views.upcoming),
+    (r'^conferences/?$', views.upcoming),
+    (r'^conferences/upcoming/?$', views.upcoming),
     (r'^conferences/upcoming/ical', views.upcomingical),
     (r'^conferences/upcoming-submissions', views.upcoming_submissions),
 )
