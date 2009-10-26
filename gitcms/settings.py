579ed80ba976b5a77790fa55397819bc8131605b
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-25 22:57:52 -0400

    Make setting more flexible.
    This allows me to deploy this copy as-is.

--- gitcms/settings.py
+++ gitcms/settings.py
@@ -12,8 +12,8 @@ APPEND_SLASH = True
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'luispedro.db'             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'                     # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = _BASE_DIR + '/luispedro.db'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
