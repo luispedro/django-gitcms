ee598b6c48b6420d0177cf80cb1bb165392b5e09
Luís Pedro Coelho <lpc@cmu.edu>
2009-10-26 14:51:42 -0400

    Convert bibtex to json using at load time
    This makes it faster, less dependent on MIT's servers and makes it easy
    to test using a non-webaccessible computer.

--- gitcms/publications/load.py
+++ gitcms/publications/load.py
@@ -1,9 +1,46 @@
 import os
+from os import path
 import shutil
+import poster
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+import urllib2
+import errno
+register_openers()
 
-_filesdir = '../media/bibtex'
+_bibfilesdir = '../media/bibtex'
+_jsfilesdir = '../media/bibtex-json'
+
+def _bibtex2json(bibtexfname):
+    datagen, headers = multipart_encode({'file': file(bibtexfname) })
+    request = urllib2.Request("http://simile.mit.edu/babel/translator?reader=bibtex&writer=bibtex-exhibit-json", datagen, headers)
+    return urllib2.urlopen(request).read()
+
+def _maybemkdir(dirname):
+    try:
+        os.mkdir(dirname)
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
 
 def loaddir(directory, clear=False):
-    if os.path.exists(_filesdir): shutil.rmtree(_filesdir)
-    shutil.copytree(directory, _filesdir)
+    if clear:
+        if path.exists(_bibfilesdir):
+            shutil.rmtree(_bibfilesdir)
+        if path.exists(_jsfilesdir):
+            shutil.rmtree(_jsfilesdir)
+    _maybemkdir(_bibfilesdir)
+    _maybemkdir(_jsfilesdir)
+    for bibfile in os.listdir(directory):
+        if bibfile[0] == '.':
+            continue
+        if not bibfile.endswith('.bib'):
+            raise ValueError, "publications: Don't know what to do with '%s'" % bibfile
+        shutil.copy(path.join(directory, bibfile), _bibfilesdir)
+        jsonfile = path.join(_jsfilesdir, bibfile[:-len('.bib')] + '.json')
+        jsonfile = file(jsonfile, 'w')
+        jsonfile.write(_bibtex2json(path.join(directory,bibfile)))
+        jsonfile.close()
+
+
 
