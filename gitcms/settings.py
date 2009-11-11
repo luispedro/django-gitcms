a9db6ead1094acf017c8dacf19ba30f783148de3
Luís Pedro Coelho <lpc@cmu.edu>
2009-11-11 12:35:40 -0500

    loadcontent.py should not be so fragile.

--- gitcms/settings.py
+++ gitcms/settings.py
@@ -95,3 +95,4 @@ INSTALLED_APPS = (
 )
 
 
+GITDJANGO_DIRNAME = '../website.content'
--- loadcontent.py
+++ loadcontent.py
@@ -1,11 +1,15 @@
 from os import listdir, path
-basedir = '../../website.content/'
-for app in listdir(basedir):
-    if app == '.git':
+import settings
+basedir = path.abspath(path.dirname(__file__))
+datadir = path.join(basedir, settings.GITDJANGO_DIRNAME)
+datadir = path.abspath(datadir)
+print datadir
+for app in listdir(datadir):
+    if app[0] == '.':
         continue
     module = __import__(app,{},{},fromlist=['load'])
     if 'loaddir' in dir(module.load):
-        module.load.loaddir(path.join(basedir, app), clear=True)
+        module.load.loaddir(path.join(datadir, app), clear=True)
     else:
         import warnings
         warnings.warn("Don't know how to import '%s'" % app)
