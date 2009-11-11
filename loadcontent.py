bf382a0bb8c8755c1b64daa5c01eecab72f73c6b
Luís Pedro Coelho <lpc@cmu.edu>
2009-11-11 15:51:25 -0500

    We shouldn't be printing a random path

--- loadcontent.py
+++ loadcontent.py
@@ -3,7 +3,6 @@ import settings
 basedir = path.abspath(path.dirname(__file__))
 datadir = path.join(basedir, settings.GITDJANGO_DIRNAME)
 datadir = path.abspath(datadir)
-print datadir
 for app in listdir(datadir):
     if app[0] == '.':
         continue
