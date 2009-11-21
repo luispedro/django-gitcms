5bc47d35b4dcae6d96a44b03665bdfd67e8ddec4
Luís Pedro Coelho <lpc@cmu.edu>
2009-11-21 05:32:43 -0500

    Implement view-by-category URL
    One last piece of the puzzle (before it was weirdly incomplete).

--- gitcms/simplecms/views.py
+++ gitcms/simplecms/views.py
@@ -1,6 +1,16 @@
 from .models import Article, Category
 from django.shortcuts import get_object_or_404, render_to_response
 
+def bycategory(request, category):
+    category =get_object_or_404(Category, slug=category)
+    articles = Article.objects.filter(categories=category)
+    return render_to_response(
+                'simplecms/list.html',
+                {
+                    'pagetitle' : ('Articles in category %s' % category.name),
+                    'articles' : articles,
+                })
+
 def article(request, url):
     article = get_object_or_404(Article, url=url)
     return render_to_response(
