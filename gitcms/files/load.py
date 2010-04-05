cebcf20e32b16b4f55bd93817aebc1c7e378a2d8
Luis Pedro Coelho <lpc@cmu.edu>
2010-04-05 08:35:39 -0400

    Make media destination configurable

--- gitcms/files/load.py
+++ gitcms/files/load.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+from django.conf import settings
 
-_filesdir = '../media/files'
+
+_defaultfilesdir = 'media/files'
+_filesdir = getattr(settings, 'DJANGO_GITCMS_FILES', _defaultfilesdir)
 
 def loaddir(directory, clear=False):
     if os.path.exists(_filesdir): shutil.rmtree(_filesdir)
