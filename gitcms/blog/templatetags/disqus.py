dde8bd07bd2a7a60f265cb535aeb5814052c0c56
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-06 14:12:58 -0500

    ENH: Make disqus optional

--- gitcms/blog/templatetags/disqus.py
+++ gitcms/blog/templatetags/disqus.py
@@ -6,11 +6,16 @@ import settings
 
 register = template.Library()
 
-@register.inclusion_tag('blog/disqus.html')
-def disqus_thread(post):
-    return {
-        'disqus_shortname' : settings.DISQUS_SHORTNAME,
-        'unique_identifier' : post.slug,
-    }
+if hasattr(settings,'DISQUS_SHORTNAME'):
+    @register.inclusion_tag('blog/disqus.html')
+    def disqus_thread(post):
+        return {
+            'disqus_shortname' : settings.DISQUS_SHORTNAME,
+            'unique_identifier' : post.slug,
+        }
+else:
+    @register.simple_tag
+    def disqus_thread(_):
+        return ''
             
     
