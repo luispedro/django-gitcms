cc40df7d9eaf76838a53ff27b98e7a238a269a13
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-06 03:43:57 -0500

    BUG: Fix regex on urls.py

--- gitcms/blog/urls.py
+++ gitcms/blog/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns('',
     (r'^tag/(?P<tag>.*)/?', views.bytag),
-    (r'^(?P<year>[0-9]+)/(?P<month>[^/]*)/(<?P<slug>[^/]*)/?', views.post),
+    (r'^(?P<year>[0-9]+)/(?P<month>[^/]*)/(?P<slug>[^/]*)/?', views.post),
     (r'^/?', views.mostrecent),
 )
