ad8e8e63f185cf8373aedcad1d313e0ecec6d1a7
Luis Pedro Coelho <luis@luispedro.org>
2011-12-29 00:09:58 +0100

    ENH Do not require /

--- gitcms/blog/urls.py
+++ gitcms/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^tag/(?P<tag>.*)/', views.bytag, name='blog-tag'),
     url(r'^(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<slug>[^/]*)/', views.post, name='blog-post'),
     url(r'^feed/', feeds.LatestFeed(), name='blog-feed'),
-    url(r'^/', views.mostrecent, name='blog'),
+    url(r'^/?$', views.mostrecent, name='blog'),
 )
